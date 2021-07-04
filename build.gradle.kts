b30a078887141ed6cbc294b466c8ccaaa4d2fd4e
Dmitriy Zhuk <dmitriy.zhuk@hiendsys.com>
2021-07-04 20:11:17 +0300

    Add liquibase configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.5.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.liquibase.gradle") version "2.0.3"
     kotlin("jvm") version "1.5.10"
     kotlin("plugin.spring") version "1.5.10"
     kotlin("plugin.jpa") version "1.5.10"
@@ -31,6 +32,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2")
+
+    liquibaseRuntime ("org.liquibase:liquibase-core:3.8.1")
+    liquibaseRuntime ("org.liquibase:liquibase-groovy-dsl:2.1.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
